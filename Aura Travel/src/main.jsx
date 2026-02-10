b8f0ba53e00e0452db2ba9c27cf23f40a83fee38
ArthurColeyX <yorkacolar@gmail.com>
2026-02-10 08:35:05 -0500

    Solucion error ReactDOM

--- Aura Travel/src/main.jsx
+++ Aura Travel/src/main.jsx
@@ -1,11 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { UserProvider } from "./context/UserContext";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <UserProvider>
-      <App />
-    </UserProvider>
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <UserProvider>
+        <App />
+      </UserProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
